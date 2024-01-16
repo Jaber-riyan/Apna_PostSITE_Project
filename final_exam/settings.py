bc9574a6be847338f1432ea15e3a3fd781c7ad5d
Jaber <jaberriyan357@gmail.com>
2024-01-16 14:47:17 +0600

    allowed hosts

--- final_exam/settings.py
+++ final_exam/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-2qx4elck*gde37*iak&5@e#f!saj*66ggfdz(*nj8wg(csybm7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
