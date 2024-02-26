d7c4c9b6344e7244aa1c2a6669f6b214d78152a1
Jaber <jaberriyan357@gmail.com>
2024-02-26 11:18:32 +0600

    admin site header add

--- final_exam/urls.py
+++ final_exam/urls.py
@@ -19,6 +19,7 @@ from django.urls import path,include
 from .import views
 from django.conf import settings
 from django.conf.urls.static import static
+admin.site.site_header="Apna Post Site Admin Panel"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
